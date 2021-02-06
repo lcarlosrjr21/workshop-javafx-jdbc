553829c3669c81fc24d2ce39d5b388170cfc5f0e
Luiz Rosa <lcarlosrjr@gmail.com>
2021-02-06 20:42:31 -0300

    Main view Controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,17 +9,16 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
 			ScrollPane scrollPane = loader.load();
-			
+
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
-			
-			
+
 			Scene mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
