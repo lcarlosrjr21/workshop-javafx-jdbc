880a16317063237fe76bc6a0f939313a447be0cf
Luiz Rosa <lcarlosrjr@gmail.com>
2021-02-09 19:10:01 -0300

    DepartmentService

---
+++ src/model/services/DepartmentService.java
@@ -0,0 +1,17 @@
+package model.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entities.Department;
+
+public class DepartmentService {
+
+	public List<Department> findAll(){
+		List<Department> list = new ArrayList<>();
+		list.add(new Department(10,"Books"));
+		list.add(new Department(11,"Computers"));
+		list.add(new Department(12,"CDs"));
+		return list;
+	}
+}
